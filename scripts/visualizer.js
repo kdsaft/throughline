f980266ae762800f5bdc74a332be0c58f65e3718
Keith <keith@whispr.ai>
2023-04-20 11:27:19 +0200

    updating rouded corners

--- scripts/visualizer.js
+++ scripts/visualizer.js
@@ -60,17 +60,17 @@ function drawBars(canvas, analyser, canvasContext) {
     ctx.beginPath();
     ctx.moveTo(x + radius, y);
     ctx.lineTo(x + width - radius, y);
-    ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+    ctx.arcTo(x + width, y, x + width, y + radius, radius);
     ctx.lineTo(x + width, y + height - radius);
-    ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+    ctx.arcTo(x + width, y + height, x + width - radius, y + height, radius);
     ctx.lineTo(x + radius, y + height);
-    ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+    ctx.arcTo(x, y + height, x, y + height - radius, radius);
     ctx.lineTo(x, y + radius);
-    ctx.quadraticCurveTo(x, y, x + radius, y);
+    ctx.arcTo(x, y, x + radius, y, radius);
     ctx.closePath();
     ctx.fill();
   }
-
+  
 // Funcation to set the canvas size to the bottom bar
 function updateCanvasSize() {
     const canvas = document.getElementById("audio-visualization");
