d84ea3e1c3b553dbaf1807a9c50499b892b2c322
Keith <keith@whispr.ai>
2023-04-20 10:28:17 +0200

    setting the visualizer basics

---
+++ scripts/visualizer.js
@@ -0,0 +1,15 @@
+
+
+
+// Call the function(s) when the window is resized
+window.addEventListener("resize", updateCanvasSize);
+  
+
+function updateCanvasSize () {
+    const canvas = document.getElementById("audio-visualization");
+    const canvasContainer = document.getElementById("canvas-container");
+
+    // Set the canvas width to the parent's width+height
+    canvas.width = canvasContainer.clientWidth;
+    canvas.height = canvasContainer.clientHeight;
+}
