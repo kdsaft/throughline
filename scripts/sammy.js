29ee2b54424a385acc5fdc19a39b25514113a146
Keith <keith@whispr.ai>
2023-04-22 14:14:47 +0200

    toggle

--- scripts/sammy.js
+++ scripts/sammy.js
@@ -1,12 +1,41 @@
 
 // opening code
+const sammy = document.getElementById("sammy");
+
 positionSammy()
 sleep();
+let isAwake = false;
 
 // When the window is resized...
 window.addEventListener("resize", positionSammy);
 
 
+
+// Add a click event listener to toggle between sleep and awake states
+
+sammy.addEventListener('click', toggleSleep);
+sammy.addEventListener('touchend', (event) => {
+    event.preventDefault();
+    toggleSleep();
+  });
+    
+
+// Functions to change Sammy's state
+
+function sleep() {
+    setState(['Beak Closed', 'Right Eye Asleep', 'Left Eye Asleep']);
+}
+
+function wakeUp() {
+    setState(['Beak Closed', 'Right Eye Awake', 'Left Eye Awake']);
+}
+
+function talk() {
+    setState(['Beak Closed', 'Beak Tongue', 'Right Eye Awake', 'Left Eye Awake']);
+}
+
+
+// Utility functions
 
 function positionSammy() {
     // Move Sammy close to the text
@@ -14,11 +43,8 @@ function positionSammy() {
     const contentRect = contentDiv.getBoundingClientRect();
     const sammyLeft = contentRect.left - 100 - 48;
 
-    const sammy = document.getElementById("sammy");
-
     sammy.style.left = sammyLeft + "px";
 }
-
 
 function setState(visibleElements) {
     const allElements = ['Beak Opened', 'Beak Tongue', 'Beak Closed', 'Right Eye Asleep', 'Right Eye Awake', 'Left Eye Asleep', 'Left Eye Awake'];
@@ -33,14 +59,14 @@ function setState(visibleElements) {
     });
 }
 
-function sleep() {
-    setState(['Beak Closed', 'Right Eye Asleep', 'Left Eye Asleep']);
-}
 
-function wakeUp() {
-    setState(['Beak Closed', 'Right Eye Awake', 'Left Eye Awake']);
-}
-
-function talk() {
-    setState(['Beak Closed', 'Beak Tongue', 'Right Eye Awake', 'Left Eye Awake']);
-}
+function toggleSleep() {
+    if (isAwake) {
+        sleep();
+      } else {
+        wakeUp();
+      }
+      isAwake = !isAwake;
+    }
+    
+    
