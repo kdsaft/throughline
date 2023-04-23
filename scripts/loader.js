1503be4261ab63f69ecd58bf8a59ed4e7fc0e200
Keith <keith@whispr.ai>
2023-04-23 17:29:25 +0200

    loading after DOM

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,25 +1,39 @@
 // loader.js
 
+
+function thingsToLoad() {
+    // CSS files
+    loadCSS('https://kdsaft.github.io/throughline/scripts/story.css');
+
+
+    // JS files
+    loadJS('https://kdsaft.github.io/throughline/scripts/reading.js');
+    loadJS('https://cdnjs.cloudflare.com/ajax/libs/howler/2.2.3/howler.min.js');
+    loadJS('https://kdsaft.github.io/throughline/scripts/visualizer.js');
+    loadJS('https://kdsaft.github.io/throughline/scripts/sammy.js');
+}
+
+
+if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", thingsToLoad);
+} else {
+    thingsToLoad();
+}
+
+
+
+
+
 function loadCSS(url) {
     var link = document.createElement('link');
     link.rel = 'stylesheet';
     link.href = url;
     document.head.appendChild(link);
-  }
-  
-  function loadJS(url) {
+}
+
+function loadJS(url) {
     var script = document.createElement('script');
     script.src = url;
     document.body.appendChild(script);
-  }
-  
-  // CSS files
-  loadCSS('https://kdsaft.github.io/throughline/scripts/story.css');
-
-  
-  // JS files
-  loadJS('https://kdsaft.github.io/throughline/scripts/reading.js');
-  loadJS('https://cdnjs.cloudflare.com/ajax/libs/howler/2.2.3/howler.min.js');
-  loadJS('https://kdsaft.github.io/throughline/scripts/visualizer.js');
-  loadJS('https://kdsaft.github.io/throughline/scripts/sammy.js');
+}
 
