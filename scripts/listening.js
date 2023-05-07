0a7fca261c5b6eaf7dfacb479defe37d7cc4fea0
Keith <keith@whispr.ai>
2023-05-07 17:13:12 +0200

    logging

--- scripts/listening.js
+++ scripts/listening.js
@@ -199,13 +199,13 @@ function highlightNextWord(wordsSpoken) {
 
     wordsArray.forEach(wordSpoken => {
         const lowercaseCurrentWord = getWordWithoutPunctuation(jsonData, currentWordNumber).toLowerCase();
-        // console.log("++ Current word:", lowercaseCurrentWord);
+         console.log("++ Current word:", lowercaseCurrentWord);
         const lowercaseWordSpoken = wordSpoken.toLowerCase();
-        // console.log("++ Word spoken:", lowercaseWordSpoken);
+        console.log("++ Word spoken:", lowercaseWordSpoken);
 
         if (lowercaseWordSpoken === lowercaseCurrentWord) {
             lastRecognizedWord = lowercaseCurrentWord;
-            // console.log("++ Match");
+             console.log("++ Match");
             readNextWord();
         }
     })
