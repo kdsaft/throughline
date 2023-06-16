fd06239a2e389590aca0edf5c7f4ad3bd82a7c8b
Keith <keith@whispr.ai>
2023-06-16 13:55:53 +0200

    update loader

--- scripts/loader.js
+++ scripts/loader.js
@@ -9,16 +9,25 @@ if (document.readyState === "loading") {
 async function thingsToLoad() {
     // CSS files
     loadCSS('https://kdsaft.github.io/throughline/scripts/story.css');
+    loadCSS('https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css');
+
+
+    if (typeof jQuery === 'undefined') {
+        document.write('<script src="https://code.jquery.com/jquery-3.6.0.min.js"><\/script>');
+    }
 
     // JS file URLs
     const scriptUrls = [
         'https://cdn.jsdelivr.net/npm/microsoft-cognitiveservices-speech-sdk@latest/distrib/browser/microsoft.cognitiveservices.speech.sdk.bundle-min.js',
         'https://cdnjs.cloudflare.com/ajax/libs/howler/2.2.3/howler.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js',
         'https://kdsaft.github.io/throughline/scripts/word.js',
         'https://kdsaft.github.io/throughline/scripts/reading.js',
         'https://kdsaft.github.io/throughline/scripts/visualizer.js',
         'https://kdsaft.github.io/throughline/scripts/listening.js',
-        'https://kdsaft.github.io/throughline/scripts/sammy.js'
+        'https://kdsaft.github.io/throughline/scripts/sammy.js',
+        'https://kdsaft.github.io/throughline/scripts/magicLens.js'
     ];
 
     try {
