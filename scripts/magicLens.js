e1eceb6551ce3b7e23ab5bb43332bb4b7449e25a
Keith <keith@whispr.ai>
2023-06-17 12:08:34 +0200

    more logging

--- scripts/magicLens.js
+++ scripts/magicLens.js
@@ -180,7 +180,7 @@ function applySpeedBump(currentLeft, currentTop, currentHeight, currentWidth) {
 
     const midpointX = currentLeft + (currentWidth / 2);
     const midpointY = currentTop + (currentHeight / 2);
-    console.log('midpoint x,y: '+ midpointX, midpointY);
+
 
     let activeClauseId = 0;
     let activeWordId = 0;
@@ -200,7 +200,8 @@ function applySpeedBump(currentLeft, currentTop, currentHeight, currentWidth) {
             bottom: $(this).offset().top - articleContainer.offset().top + $(this).height()
         };
 
-        console.log('clause: ' + clause.left, clause.right, clause.top, clause.bottom);
+        console.log('clause width: ' + clause.left  + ' - ' + midpointX + ' - ' + clause.right );
+        console.log('clause height: ' + clause.top  + ' - ' + midpointY + ' - ' + clause.bottom );
 
         if (midpointX > clause.left && midpointX < clause.right && midpointY > clause.top && midpointY < clause.bottom) {
             activeClauseId = $(this).attr("id").split("-")[1];
